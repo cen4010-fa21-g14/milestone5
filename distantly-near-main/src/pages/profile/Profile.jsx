9cb588fce246b71de1022c31cd3f935ffd40f26b
andresbotia <abotia2018@fau.edu>
2021-12-02 19:20:06 -0500

    my first commit

--- distantly-near-main/src/pages/profile/Profile.jsx
+++ distantly-near-main/src/pages/profile/Profile.jsx
@@ -26,7 +26,7 @@ export default function Profile() {
         <>
         <Topbar/>
         <div className="profile">
-            <Sidebar/>
+            {/* <Sidebar/> */}
             <div className="profileRight">
                 <div className="profileRightTop">
                     <div className="profileCover">
