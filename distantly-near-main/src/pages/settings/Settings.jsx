6d2ddbd1c7b5adf56e48199770f80dcc6edd306f
andresbotia <abotia2018@fau.edu>
2021-12-03 17:58:31 -0500

    my first commit

--- distantly-near-main/src/pages/settings/Settings.jsx
+++ distantly-near-main/src/pages/settings/Settings.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Topbar from "../../components/topbar/Topbar";
-
+import "./settings.css";
 export default function Settings() {
     return (
         <div>
             <>
             <Topbar/>
-
+            
            </>
         </div>
     )
