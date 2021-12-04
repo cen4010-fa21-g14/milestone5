b68cf85d420bfcd077dd1294798a64637e3e71cc
andresbotia <abotia2018@fau.edu>
2021-12-03 21:51:27 -0500

    my first commit

--- distantly-near-main/src/components/rightbar/Rightbar.jsx
+++ distantly-near-main/src/components/rightbar/Rightbar.jsx
@@ -49,15 +49,48 @@ export default function Rightbar({ user }) {
   const HomeRightbar = () => {
     return (
       <>
-        {/* <div className="birthdayContainer">
-          <img className="birthdayImg" src={`${PF}birthday.png`} alt="" />
-          <span className="birthdayText">
-            <b>Pola Foster</b> and <b>3 other friends</b> have a birhday today.
-          </span>
+  {user.username == currentUser.username && (
+                            <Link to="/settings" style={{textDecoration:"none"}}>
+
+                            <button className="button" >
+                                Settings
+                              </button>
+                            </Link>
+        )}
+
+        {user.username !== currentUser.username && (
+        <button className="rightbarFollowingButton" onClick={handleClick}>
+          {followed ? "Unfollow" : "Follow"}
+          {followed ? <Remove /> : <Add />}
+          </button>
+          )}
+        <h4 className="rightbarTitle">User information</h4>
+        <div className="rightbarInfo">
+        <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">Username:</span>
+            <span className="rightbarInfoValue">{user.username}</span>
+          </div>
+        <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">Name:</span>
+            <span className="rightbarInfoValue">{user.firstName} {user.lastName}</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">City:</span>
+            <span className="rightbarInfoValue">{user.city}</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">From:</span>
+            <span className="rightbarInfoValue">{user.from}</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">Relationship:</span>
+            <span className="rightbarInfoValue">{user.relationship ===1 
+                                                  ? "Single" : user.relationship ===2 
+                                                  ? "Married"
+                                                  : "-"}</span>
+          </div>
         </div>
-        <img className="rightbarAd" src={`${PF}ad.png`}  alt="" /> */}
-        <h4 className="rightbarTitle">Online Friends</h4>
-        <ul className="rightbarFriendList">
+        <h4 className="rightbarTitle">User friends</h4>
         <div className="rightbarFollowings">
           {friends.map((friend) => (
             <Link
@@ -79,7 +112,6 @@ export default function Rightbar({ user }) {
             </Link>
           ))}
         </div>
-        </ul>
       </>
     );
   };
