d6272453179501dc58f805ba4bea86c12ee3a5c4
andresbotia <abotia2018@fau.edu>
2021-12-04 20:40:03 -0500

    milestone5 V2

--- distantly-near-main/src/pages/settings/Settings.jsx
+++ distantly-near-main/src/pages/settings/Settings.jsx
@@ -6,23 +6,21 @@ export default function Settings() {
         <div>
             <>
             <Topbar/>
-            <div className="login">
-            <div className="loginWrapper">
-                <div className="loginLeft">
-                    <h3 className="loginLogo">Distantly-Near</h3>
-                    <span className="loginDesc">Closer than you think!</span>
+            <div className="">
+            <div className="">
+                <div className="">
+                    <h3 className="">Distantly-Near</h3>
+                    <span className="">Closer than you think!</span>
                 </div>
-                <div className="loginRight">
-                    <form className="loginBox" onSubmit={handleClick}>
+                <div className="">
+                    <form className="" >
                         <input placeholder="Username" required ref={username} className="loginInput" />
                         <input placeholder="Email" required ref={email} className="loginInput" type="email"/>
                         <input placeholder="Password" required ref={password} className="loginInput" type="password" minLength="6"/>
                         <input placeholder="Password Again" required ref={passwordAgain} className="loginInput" type="password"/>
-                        <button className="loginButton" type="submit">Sign Up</button>
-                        <button className="loginRegisterButton">
-                        <Link to="/login" style={{textDecoration: 'none', color:'white',display: 'block'}}>
-                        Log into Account
-                        </Link>
+                        <button className="" type="submit">Sign Up</button>
+                        <button className="">
+                        
                             </button>                    </form>
                 </div>
             </div>
