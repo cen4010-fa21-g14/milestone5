7f0f261b8f8270011fe523c90c8331bcbfc1c31c
andresbotia <abotia2018@fau.edu>
2021-12-05 17:43:54 -0500

    milestone5 V2

--- distantly-near-main/src/components/post/Post.jsx
+++ distantly-near-main/src/components/post/Post.jsx
@@ -68,7 +68,10 @@ export default function Post({post}) {
                 <div className="postBottom">
                     <div className="postBottomLeft">
                         
-                        <img className="likeIcon" alt="" src={`${PF}heart.png`} onClick={likeHandler}></img>
+                    <img className="likeIcon" alt="" 
+                        src=
+                        {isLiked ? `${PF}heart.png` : `${PF}noHeart.png`}
+                         onClick={likeHandler}></img>
                         <span className="postLikeCounter">{like} people like it </span>
                     </div>
                     {/* <div className="postBottomRight">
