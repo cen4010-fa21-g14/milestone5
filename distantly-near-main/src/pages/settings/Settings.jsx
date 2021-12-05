87a52f79398199d765180820bcf33361ad02aa8b
andresbotia <abotia2018@fau.edu>
2021-12-04 20:52:45 -0500

    milestone5 V2

--- distantly-near-main/src/pages/settings/Settings.jsx
+++ distantly-near-main/src/pages/settings/Settings.jsx
@@ -6,22 +6,20 @@ export default function Settings() {
         <div>
             <>
             <Topbar/>
-            <div className="">
-            <div className="">
-                <div className="">
-                    <h3 className="">Distantly-Near</h3>
-                    <span className="">Closer than you think!</span>
+            <div className="settings">
+            <div className="settingsWrapper">
+                <div className="settingsLeft">
+                    <h3 className="settingsLogo">Distantly-Near</h3>
+                    <span className="settingsDesc">Closer than you think!</span>
                 </div>
-                <div className="">
-                    <form className="" >
+                <div className="SettingsRight">
+                    <form className="SettingsBox" >
                         <input placeholder="Username" className="loginInput" />
                         <input placeholder="Email" className="loginInput" type="email"/>
                         <input placeholder="Password" className="loginInput" type="password" minLength="6"/>
                         <input placeholder="Password Again"  className="loginInput" type="password"/>
-                        <button className="" type="submit">Sign Up</button>
-                        <button className="">
-                        
-                            </button>                    </form>
+                        <button className="button"></button>                    
+                        </form>
                 </div>
             </div>
         </div>
