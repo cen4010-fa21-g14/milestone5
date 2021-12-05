683a1139e9fa5af9f67b9ccc9d3c44ce0319cbe5
andresbotia <abotia2018@fau.edu>
2021-12-04 20:37:24 -0500

    milestone5 V2

--- distantly-near-main/src/pages/settings/Settings.jsx
+++ distantly-near-main/src/pages/settings/Settings.jsx
@@ -6,7 +6,27 @@ export default function Settings() {
         <div>
             <>
             <Topbar/>
-            
+            <div className="login">
+            <div className="loginWrapper">
+                <div className="loginLeft">
+                    <h3 className="loginLogo">Distantly-Near</h3>
+                    <span className="loginDesc">Closer than you think!</span>
+                </div>
+                <div className="loginRight">
+                    <form className="loginBox" onSubmit={handleClick}>
+                        <input placeholder="Username" required ref={username} className="loginInput" />
+                        <input placeholder="Email" required ref={email} className="loginInput" type="email"/>
+                        <input placeholder="Password" required ref={password} className="loginInput" type="password" minLength="6"/>
+                        <input placeholder="Password Again" required ref={passwordAgain} className="loginInput" type="password"/>
+                        <button className="loginButton" type="submit">Sign Up</button>
+                        <button className="loginRegisterButton">
+                        <Link to="/login" style={{textDecoration: 'none', color:'white',display: 'block'}}>
+                        Log into Account
+                        </Link>
+                            </button>                    </form>
+                </div>
+            </div>
+        </div>
            </>
         </div>
     )
