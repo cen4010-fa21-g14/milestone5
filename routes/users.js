f3c2b3cf3ef6de934f5a7dddfc722ba50e5368b3
andresbotia <abotia2018@fau.edu>
2021-12-04 21:54:56 -0500

    milestone5 V2

--- routes/users.js
+++ routes/users.js
@@ -52,7 +52,7 @@ router.delete("/:id", async (req,res)=>{
 
 //Get a user
 
-router.get("/:id", async(req,res)=>{
+router.get("/", async(req,res)=>{
   const userId = req.query.userId;
   const username = req.query.username;
 
